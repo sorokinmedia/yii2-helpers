316b8c554391bff30097c7878bf5a2464671f1d3
ma3obblu <Ma3oBblu@gmail.com>
2018-08-09 10:31:05 +0200

    add api array method

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -9,5 +9,20 @@ namespace sorokinmedia\helpers;
  */
 class ArrayHelper
 {
-    
+    /**
+     * конвертит массив key=>value в массив объектов {'id', 'name'}
+     * @param array $array
+     * @return array
+     */
+    public static function convertArrayToArrayOfObject(array $array) : array
+    {
+        $extended_array = [];
+        foreach ($array as $key => $value){
+            $extended_array[] = [
+                'id' => $key,
+                'name' => $value,
+            ];
+        }
+        return $extended_array;
+    }
 }
